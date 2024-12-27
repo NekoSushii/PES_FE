2cbc9832ce5bf76e804444b136c621b4c768dd78
Pang Hing Tay (Philip) <PaTay@eagle.org>
2024-12-27 12:37:10 +0800

    d28 done

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { GoogleMap, LoadScript, Polygon } from "@react-google-maps/api";
+import { GoogleMap, LoadScript, Marker, Polygon } from "@react-google-maps/api";
 import { polygons } from "../polygons/districtConfig";
 
 const containerStyle = {
@@ -28,30 +28,44 @@ const Map: React.FC = () => {
 
   return (
     <LoadScript
-      // googleMapsApiKey="AIzaSyCQWZOvyYbdhpAUfP00vB_XEbg4XY4WwF0"
-      googleMapsApiKey="asdasdasdasd"
+      googleMapsApiKey="AIzaSyCQWZOvyYbdhpAUfP00vB_XEbg4XY4WwF0"
+      // googleMapsApiKey="asdasdasdasd"
       onLoad={() => setIsLoaded(true)}
     >
       {isLoaded && polygonData && (
         <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={12}>
-          {polygonData.map((polygon) => (
-            <Polygon
-              key={polygon.id}
-              paths={polygon.path}
-              options={{
-                fillColor: polygon.fillColor,
-                fillOpacity: 0.4,
-                strokeColor: polygon.fillColor,
-                strokeOpacity: 0.8,
-                strokeWeight: 2,
-                clickable: true,
-                draggable: false,
-                editable: false,
-              }}
-              onClick={() => handlePolygonClick(polygon.name)}
-            />
-          ))}
-        </GoogleMap>
+        {polygonData.map((polygon) => (
+          <Polygon
+            key={polygon.id}
+            paths={polygon.path}
+            options={{
+              fillColor: polygon.fillColor,
+              fillOpacity: 0.4,
+              strokeColor: polygon.fillColor,
+              strokeOpacity: 0.8,
+              strokeWeight: 2,
+              clickable: true,
+              draggable: false,
+              editable: false,
+            }}
+            onClick={() => handlePolygonClick(polygon.name)}
+          />
+        ))}
+        
+        {polygonData.map((polygon) => (
+          <Marker
+            key={`label-${polygon.id}`}
+            position={polygon.center}
+            label={{
+              text: polygon.name,
+              color: "black",
+              fontSize: "14px",
+              fontWeight: "bold",
+            }}
+            options={{ icon: { path: google.maps.SymbolPath.CIRCLE, scale: 0 } }}
+          />
+        ))}
+      </GoogleMap>
       )}
     </LoadScript>
   );
--- src/polygons/districtConfig.ts
+++ src/polygons/districtConfig.ts
@@ -2,64 +2,88 @@ export interface PolygonConfig {
     id: number;
     name: string;
     path: { lat: number; lng: number }[];
+    center: { lat: number; lng: number };
     fillColor: string;
   }
   
   export const polygons: PolygonConfig[] = [
+    // {
+    //   id: 1,
+    //   name: "District 1",
+    //   path: [
+    //     { lat: 1.345634, lng: 103.881787 },
+    //     { lat: 1.346124, lng: 103.882860 },
+    //     { lat: 1.346280, lng: 103.883522 },
+    //     { lat: 1.346068, lng: 103.883647 },
+    //     { lat: 1.345038, lng: 103.882264 },
+    //   ],
+    //   fillColor: "#FF0000",
+    // },
+    // {
+    //   id: 2,
+    //   name: "District 2",
+    //   path: [
+    //     { lat: 1.3521, lng: 103.8198 },
+    //     { lat: 1.3530, lng: 103.8210 },
+    //     { lat: 1.3510, lng: 103.8230 },
+    //     { lat: 1.3500, lng: 103.8200 },
+    //   ],
+    //   fillColor: "#00FF00",
+    // },
+    // {
+    //   id: 3,
+    //   name: "District 3",
+    //   path: [
+    //     { lat: 1.3591, lng: 103.8108 },
+    //     { lat: 1.3601, lng: 103.8128 },
+    //     { lat: 1.3581, lng: 103.8138 },
+    //     { lat: 1.3571, lng: 103.8118 },
+    //   ],
+    //   fillColor: "#0000FF",
+    // },
+    // {
+    //   id: 4,
+    //   name: "District 4",
+    //   path: [
+    //     { lat: 1.3491, lng: 103.8008 },
+    //     { lat: 1.3501, lng: 103.8028 },
+    //     { lat: 1.3481, lng: 103.8038 },
+    //     { lat: 1.3471, lng: 103.8018 },
+    //   ],
+    //   fillColor: "#FFFF00",
+    // },
     {
-      id: 1,
-      name: "District 1",
+      id: 28,
+      name: "District 28\n\nSeletar",
       path: [
-        { lat: 1.345634, lng: 103.881787 },
-        { lat: 1.346124, lng: 103.882860 },
-        { lat: 1.346280, lng: 103.883522 },
-        { lat: 1.346068, lng: 103.883647 },
-        { lat: 1.345038, lng: 103.882264 },
+        { lat: 1.432527, lng: 103.875752 },
+        { lat: 1.423533, lng: 103.862885 },
+        { lat: 1.407684, lng: 103.850567 },
+        { lat: 1.39875, lng: 103.856515 },
+        { lat: 1.391318, lng: 103.858093 },
+        { lat: 1.392069, lng: 103.850899 },
+        { lat: 1.391259, lng: 103.851006 },
+        { lat: 1.391108, lng: 103.850297 },
+        { lat: 1.387562, lng: 103.850671 },
+        { lat: 1.387664, lng: 103.854749 },
+        { lat: 1.3877, lng: 103.854841 },
+        { lat: 1.38518, lng: 103.857272 },
+        { lat: 1.385207, lng: 103.858335 },
+        { lat: 1.376645, lng: 103.858501 },
+        { lat: 1.378426, lng: 103.8657 },
+        { lat: 1.378928, lng: 103.876637 },
+        { lat: 1.38303, lng: 103.87689 },
+        { lat: 1.385014, lng: 103.878472 },
+        { lat: 1.386114, lng: 103.880296 },
+        { lat: 1.390353, lng: 103.881649 },
+        { lat: 1.393563, lng: 103.881854 },
+        { lat: 1.395285, lng: 103.88475 },
+        { lat: 1.403982, lng: 103.887547 },
+        { lat: 1.413229, lng: 103.896073 },
+        { lat: 1.420946, lng: 103.900041 },
+        { lat: 1.434348, lng: 103.87691 },
       ],
-      fillColor: "#FF0000",
-    },
-    {
-      id: 2,
-      name: "District 2",
-      path: [
-        { lat: 1.3521, lng: 103.8198 },
-        { lat: 1.3530, lng: 103.8210 },
-        { lat: 1.3510, lng: 103.8230 },
-        { lat: 1.3500, lng: 103.8200 },
-      ],
-      fillColor: "#00FF00",
-    },
-    {
-      id: 3,
-      name: "District 3",
-      path: [
-        { lat: 1.3591, lng: 103.8108 },
-        { lat: 1.3601, lng: 103.8128 },
-        { lat: 1.3581, lng: 103.8138 },
-        { lat: 1.3571, lng: 103.8118 },
-      ],
-      fillColor: "#0000FF",
-    },
-    {
-      id: 4,
-      name: "District 4",
-      path: [
-        { lat: 1.3491, lng: 103.8008 },
-        { lat: 1.3501, lng: 103.8028 },
-        { lat: 1.3481, lng: 103.8038 },
-        { lat: 1.3471, lng: 103.8018 },
-      ],
-      fillColor: "#FFFF00",
-    },
-    {
-      id: 5,
-      name: "District 5",
-      path: [
-        { lat: 1.3421, lng: 103.8308 },
-        { lat: 1.3431, lng: 103.8328 },
-        { lat: 1.3411, lng: 103.8338 },
-        { lat: 1.3401, lng: 103.8318 },
-      ],
+      center: { lat: 1.407387, lng: 103.872199},
       fillColor: "#FF00FF",
     },
   ];
