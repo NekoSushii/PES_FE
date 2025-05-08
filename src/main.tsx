d31d669d4e3a08b370533dcbfa2f8fe4f12fca11
Philip <philiptay97@gmail.com>
2025-05-08 13:57:59 +0800

    LogRocket

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+import LogRocket from 'logrocket';
+import { v4 as uuid } from 'uuid';
+
+LogRocket.init('gonje5/pes-e0bgm');
+
+const STORAGE_KEY = 'lr_anonymous_id';
+
+let anonId = localStorage.getItem(STORAGE_KEY);
+if (!anonId) {
+  anonId = uuid();
+  localStorage.setItem(STORAGE_KEY, anonId);
+}
+
+LogRocket.identify(anonId, { anonymous: true });
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
